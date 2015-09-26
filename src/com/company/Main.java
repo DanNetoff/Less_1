a03330d7d0edc19cb8a129ba03932ac470f5cc74
DanNetoff <danil_naik@me.com>
2015-09-26 14:03:23 +0300

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,82 @@
+package com.company;
+
+import com.sun.org.apache.xpath.internal.SourceTree;
+
+import java.io.IOException;
+import java.util.DoubleSummaryStatistics;
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+    /*
+        int x;
+        x = System.in.read();
+        char c = (char) x;
+        System.out.println(x +"="+ c);
+
+        System.out.println("INPUT NAME :");
+        Scanner scan = new Scanner(System.in);
+       // scan = System.in;
+
+        String name = scan.next();
+        System.out.println("Hi" + name);
+        scan.close();
+
+
+
+        int a=7, b=2;
+        System.out.println(a/b);
+
+
+        int a=7;
+
+        System.out.println(a++);
+        System.out.println(++a);
+
+
+
+        float d = 0.10f;
+        double dd = .5;
+
+        float x=0, y=0;
+        x = (float) (0.3 + 0.4);
+        y = 0.3f + 0.4f;
+        System.out.println(x +"="+ y);
+        System.out.println(x==y);
+
+
+        int a = 7;
+        double b = 0.0;
+        double c = -0.0;
+        double d = 2.5;
+        double g = Double.NEGATIVE_INFINITY;
+
+        System.out.println(a/b==Double.NEGATIVE_INFINITY);
+        System.out.println(a/c==Double.NEGATIVE_INFINITY);
+        System.out.println(b==c);
+        System.out.println(d/0);
+        System.out.println(g*0);
+
+
+        char a = '1';
+        char b = '\u0031';
+        char c = 49;
+
+        System.out.println(a+""+b+""+ c);
+
+       // System.out.println("4/2= " + 4/2 == "4/2=2");
+
+        String a = "a";
+        String b = "a";
+        System.out.println(a==b);
+
+        a = "c";
+        System.out.println(a==b);
+        b ="c";
+        System.out.println(a==b);
+
+*/
+
+    }
+}
